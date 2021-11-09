c108ec3da2d5b1758c6231624ab3891d66f26458
ram679 <chramanji548@gmail.com>
2021-11-08 19:26:42 -0800

    ram1

---
+++ main1.py
@@ -0,0 +1,21 @@
+
+# Python3 program for the above approach
+import math
+ 
+def checkperfectsquare(x):
+     
+    # If ceil and floor are equal
+    # the number is a perfect
+    # square
+    if (math.ceil(math.sqrt(n)) ==
+       math.floor(math.sqrt(n))):
+        print("perfect square")
+    else:
+        print("not a perfect square")
+     
+# Driver code
+n = 49
+  
+checkperfectsquare(n)
+ 
+# This code is contributed by jana_sayantan
